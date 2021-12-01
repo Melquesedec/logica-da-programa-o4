3c3d9dec0e7316150118d6d756ec2417aa2181b6
Melquesedec <melquem2005@gmail.com>
2021-11-30 21:50:20 -0300

    aula06

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -43,11 +43,67 @@ function resultado4(){
     let media = parseInt(document.getElementById('media').value);
     let resultadoMedia = document.getElementById('resultado4');
     
-    if(media >= 7){
-        resultadoMedia.innerHTML=(`Você foi aprovado, sua nota é ${media}`)
+    if(media == 7){
+        resultadoMedia.innerHTML=(`Você foi aprovado, sua nota é <strong>${media}!</strong>`)
     }
-    if(media <= 6){
-        resultadoMedia.innerHTML=(`Você foi reprovado, sua nota é ${media}`)
+    else if(media == 6){
+        resultadoMedia.innerHTML=(`Você foi reprovado, sua nota é <strong>${media}!</strong>`)
+    }
+    else if(media == 10){
+        resultadoMedia.innerHTML=(`Você foi aprovado com <strong>DISTINÇÃO!</strong>, sua nota é <strong>${media}!</strong>`)
+    }
+    else if(media == 5){
+        resultadoMedia.innerHTML=(`Você foi reprovado, sua nota é <strong>${media}!</strong>`)
+    }
+    else if(media == 4){
+        resultadoMedia.innerHTML=(`Você foi reprovado, sua nota é <strong>${media}!</strong>`)
+    }
+    else if(media == 3){
+        resultadoMedia.innerHTML=(`Você foi reprovado, sua nota é <strong>${media}!</strong>`)
+    }
+    else if(media == 2){
+        resultadoMedia.innerHTML=(`Você foi reprovado, sua nota é <strong>${media}!</strong>`)
+    }
+    else if(media == 1){
+        resultadoMedia.innerHTML=(`Você foi reprovado, sua nota é <strong>${media}!</strong>`)
+    }
+    else if(media == 0){
+        resultadoMedia.innerHTML=(`Você foi reprovado, sua nota é <strong>${media}!</strong>`)
+    }
+    else if(media == 8){
+        resultadoMedia.innerHTML=(`Você foi aprovado, sua nota é <strong>${media}!</strong>`)
+    }
+    else if(media == 9){
+        resultadoMedia.innerHTML=(`Você foi aprovado, sua nota é <strong>${media}!</strong>`)
+    }
     }
 
+    function resultado5(){
+        let cons = String(document.getElementById('cons').value);
+        let resultado5 = document.getElementById('resultado5');
+        
+        if(cons == 'b' || cons == 'c' ||cons == 'd' ||cons == 'f' ||cons == 'g' ||cons == 'j' ||cons == 'k' ||cons == 'l' ||cons == 'm'
+         ||cons == 'n' ||cons == 'p' ||cons == 'q' ||cons == 'r' ||cons == 's' ||cons == 't' ||cons == 'v' ||cons == 'w' || cons == 'x' 
+         || cons == 'z'){
+            resultado5.innerHTML=(`Você digitou uma consoante`);
+        }else{
+            resultado5.innerHTML=(`Você digitou uma vogal`)
+    } 
+    }
+
+    function resultado6(){
+        let maior = parseInt(document.getElementById('maior').value);
+        let menor = parseInt(document.getElementById('menor').value);
+        let meio = parseInt(document.getElementById('meio').value);
+        let resultado6 = document.getElementById('resultado6');
+
+        if( maior > menor <= meio){       
+            resultado6.innerHTML=(`Maior número é ${maior} e o menor ${menor}`)
+        }
+        else if( maior <= menor > meio){       
+            resultado6.innerHTML=(`Maior número é ${maior} e o menor ${menor}`)
+        }
+        
 }
+
+
